5cd8e7ccf6449e678f1348d721daaac6da07348f
Arlan Moura <arlan_moura@hotmail.com>
2023-10-28 19:56:24 -0300

    alterando arquivo de exemplo1

--- scripts/exemplo1.js
+++ scripts/exemplo1.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+//import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 //smoke test
 export const options = {
@@ -21,8 +21,8 @@ export default function(){
     })
 }
 
-export function handleSummary(data) {
+/*export function handleSummary(data) {
     return {
       "teste_k6_1.html": htmlReport(data),
     };
-  }
+  }*/
